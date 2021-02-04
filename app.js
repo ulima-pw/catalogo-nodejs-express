912ef4dac85c11a0a7472f3b27793c516c2eb092
Hernan <hernan@jsatch.com>
2021-02-04 16:42:03 -0500

    sistema de templates e if explicados

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.use(bodyParser.urlencoded({
     extended : true
 }))
 
+app.set('view engine', 'ejs');
+
 // Endpoint
 app.get('/banner', (req, res) => {
     res.send(JSON.stringify(data.imagenes));
@@ -39,6 +41,32 @@ app.get('/usuarios', (req, res) => {
 // Endpoint que retorna paises
 app.get('/paises' , (req, res) => {
     res.send(JSON.stringify(data.paises))
+})
+
+// Endpoint mostrar el formulario login
+app.get('/login', (req, res) => {
+    // Llamando al motor de templates diciendo que cree un html
+    // en base al template login.ejs y luego el resultado se
+    // devuelve al cliente.
+    res.render('login', {
+        error : false
+    });
+})
+
+// Endpoint procesar el formulario para ver si login se acepta
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username == "pw" && password == "123") {
+        // Login correcto
+        res.redirect('/catalogo.html')
+    } else {
+        // Login incorrecto
+        res.render('login', {
+            error : true
+        })
+    }
 })
 
 
