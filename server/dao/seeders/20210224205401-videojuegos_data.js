9570bf2624bc9af0ba61afe0c9103f632d8502ce
Hernan <hernan@jsatch.com>
2021-02-24 15:57:36 -0500

    seed de videojuegos

---
+++ server/dao/seeders/20210224205401-videojuegos_data.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Videojuego', [
+      {
+        nombre : "Demons Soul",
+        precio : 30,
+        categoriaId : 1
+      }
+    ])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Videojuego', null);
+  }
+};
